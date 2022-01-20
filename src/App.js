d94506cc6b8d7b613dcbf5441246bee9862f8564
carmenmendez <carmenmendez.github.io>
2022-01-20 13:39:08 -0600

    implementando el estado del input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import React from 'react';
 import './App.css';
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      newTodo: '',
+    }
+  }
+  render(){
+    return(
+      <main>
+        <div>
+          <p>Por hacer: 0</p>
+          <input
+            type="text" 
+            value={this.state.newTodo} 
+            onChange={(event) => {
+              this.setState({ newTodo: event.target.value}) 
+            }}
+          />
+          <button>+</button>
+        </div>
+        <div>
+          <ul>
+            <li>Tarea <button>x</button></li>
+            <li>Tarea <button>x</button></li>
+          </ul>
+        </div>
+      </main>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      
+      <TodoList/>
     </div>
   );
 }
